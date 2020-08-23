7d19f5d39f5652925a0f89bddc5d92a0d7a6b993
Stacy Hung <sshung@gmail.com>
2020-08-22 18:14:59 -0700

    fix snakegame

--- games/snakegame.js
+++ games/snakegame.js
@@ -2,7 +2,7 @@ let score = 0;
 let scoreDisplay = document.getElementById("score");
 let startBtn = document.getElementById("start-btn");
 let grid = document.getElementById("grid");
-let gridWidth = 20; // each square should be 25 x 25px
+let width = 20; // each square should be 25 x 25px
 let direction = 1; // 1: right; -1: left; width: up; -width: down
 let squares = [];
 let currentSnake = [2, 1, 0]; // head is based on grid number moving right
@@ -13,13 +13,13 @@ let appleIndex = 0;
 
 // create grid
 function createGrid() {
-    for (let i = 0; i < gridWidth * gridWidth; i++) {
+    for (let i = 0; i < width * width; i++) {
         // create a new div representing a square in the grid
-        let gridSquare = document.createElement("div");
+        let square = document.createElement("div");
         // add styling to the square
-        gridSquare.classList.add('square');
+        square.classList.add('square');
         // add the square to the grid
-        grid.appendChild(gridSquare);
+        grid.appendChild(square);
         // add square to the squares array
         squares.push(square);
     }
@@ -29,6 +29,7 @@ createGrid();
 currentSnake.forEach(index => squares[index].classList.add('snake'));
 
 function startGame() {
+    console.log("restarting game!");
     // remove the snake
     currentSnake.forEach(index => squares[index].classList.remove('snake'));
 
@@ -47,13 +48,14 @@ function startGame() {
     // re-add the class of snake to the new currentSnake
     currentSnake = [2, 1, 0];
     direction = 1;
-    currentSnake.forEach(index => squares[index].classList.add('snake'));
-    setInterval(move, timerId);
     generateApple();
+    currentSnake.forEach(index => squares[index].classList.add('snake'));
+    timerId = setInterval(move, interval);
 }
 
 
 function move() {
+    console.log("A move!");
     // if the snake is going to hit the wall, stop
     /*
         Snake will hit the top wall: index - width < 0 and moving up
@@ -61,10 +63,13 @@ function move() {
         Snake will hit the bottom wall: index + width < width * width and moving down
         Snake will hit the left wall: index % width = 0 and moving right
     */
-    if ((currentSnake[0] - width < 0 && direction === width) ||
-        (currentSnake[0] % width === width - 1 && direction === -1) ||
-        (currentSnake[0] + width >= width * width && direction === -width) ||
-        (currentSnake[0] % width === 0 && direction === 1)) {
+    if ((currentSnake[0] - width < 0 && direction === -width) ||
+        (currentSnake[0] % width === width - 1 && direction === 1) ||
+        (currentSnake[0] + width >= width * width && direction === width) ||
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (squares[currentSnake[0] + direction].classList.contains('snake'))
+    ) {
+        console.log('Snake is going to hit a wall!');
         clearInterval(timerId);
     }
     // otherwise remove last element (and styling) from snake array
@@ -75,6 +80,8 @@ function move() {
     currentSnake.unshift(currentSnake[0] + direction);
 
     // handle the case when the snake hits an apple
+    console.log('Index of currentSnake[0]: ' + currentSnake[0]);
+
     if (squares[currentSnake[0]].classList.contains('apple')) {
         // remove class apple
         squares[currentSnake[0]].classList.remove('apple');
@@ -89,7 +96,8 @@ function move() {
         scoreDisplay.textContent = score;
         // speed up the snake:
         //      1. reset timerId
-        timerId = 0;
+        clearInterval(timerId);
+        // timerId = 0;
         //      2. decrease interval time
         interval = interval * speed;
         //      3. start new interval
@@ -102,7 +110,7 @@ function generateApple() {
     // find a suitable random location to place the apple - i.e. not where the snake is located
     do {
         appleIndex = Math.floor(Math.random() * width * width);
-    } while (squares[applexIndex].classList.contains('snake'))
+    } while (squares[appleIndex].classList.contains('snake'))
     // add styling
     squares[appleIndex].classList.add('apple');
 }
@@ -115,13 +123,17 @@ Change direction of snake based on arrow key pressed
     40: down
 */
 function control(event) {
-    switch (event) {
+    // console.log("changing direction!");
+    /*switch (event.keyCode) {
         case 37: direction = -1;
-        case 38: direction = width;
+        case 38: direction = -width;
         case 39: direction = 1;
-        case 40: direction = -width;
-    }
+        case 40: direction = width;
+    }*/
+    if (event.keyCode === 37) { direction = -1; }
+    else if (event.keyCode === 38) { direction = -width; }
+    else if (event.keyCode === 39) { direction = 1; }
+    else if (event.keyCode === 40) { direction = width; }
 }
-
 document.addEventListener("keyup", control);
 startBtn.addEventListener("click", startGame);
