737628127bb9afc721ff9f03eab58145a0a5cbb0
Stacy Hung <sshung@gmail.com>
2020-09-05 10:05:13 -0700

    add submit listener for email address

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var header = document.getElementById("myHeader");
-console.log("The header is " + header.innerHTML);
-
 // modify navigation button style
 
 /*
@@ -22,17 +19,33 @@ for (var i = 0; i < navButtons.length; i++) {
 // Event Listeners
 
 // When the user clicks on the input box
-var inputBox = document.getElementById("guest-input");
-inputBox.addEventListener("click", function () {
-    this.value = "";
-    this.style.color = "black";
-});
+// var inputBox = document.getElementById("guest-input");
+// inputBox.addEventListener("click", function () {
+//     this.value = "";
+//     this.style.color = "black";
+//     console.log("thank you for your input!")
+// });
 
-// When the user clicks the submit button
-var submitBtn = document.getElementById("submit-button");
-submitBtn.addEventListener("click", function () {
-    //clear the text box and bring up a text saying thank you <guest name>
-    var guestName = document.getElementById("guest-input").value;
-    document.getElementById("guest-input").value = "";
-    document.getElementById("thankyou").textContent = "Thank you " + guestName + "!";
-})
+// // When the user clicks the submit button
+// var submitBtn = document.getElementById("submit-button");
+// submitBtn.addEventListener("click", function () {
+//     //clear the text box and bring up a text saying thank you <guest name>
+//     var guestName = document.getElementById("guest-input").value;
+//     document.getElementById("guest-input").value = "";
+//     document.getElementById("thankyou").textContent = "Thank you " + guestName + "!";
+// });
+
+// Produce a message when the user enters a valid email
+let submitForm = document.getElementById("suscribe-form");
+submitForm.addEventListener("submit", event => {
+    // prevent default behavior
+    event.preventDefault();
+
+    // get user's email from the form
+    let formData = new FormData(event.target);
+    let userEmail = formData.get("email-address");
+    console.log("Got email address: " + userEmail);
+
+    let updatedContent = `<p>Congratulations, ${userEmail} has been added! Keep an eye out for email updates!</p>`;
+    submitForm.innerHTML = updatedContent;
+});
