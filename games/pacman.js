8fee354d622b38039c79c80cec46f76bee538895
Stacy Hung <sshung@gmail.com>
2020-09-11 09:05:40 -0700

    add ghosts to grid

--- games/pacman.js
+++ games/pacman.js
@@ -149,3 +149,24 @@ function eatPacDot() {
         squares[pacmanCurrentIndex].classList.remove('pac-dot');
     }
 }
+
+class Ghost {
+    constructor(className, startIndex, speed) {
+        this.className = className;
+        this.startIndex = startIndex;
+        this.speed = speed;
+    }
+
+}
+
+let ghosts = [
+    new Ghost('blinky', 348, 250),
+    new Ghost('pinky', 376, 400),
+    new Ghost('inky', 351, 300),
+    new Ghost('clyde', 379, 500)
+];
+
+//draw ghosts onto the grid
+ghosts.forEach(ghost => {
+    squares[ghost.startIndex].classList.add(ghost.className);
+});
