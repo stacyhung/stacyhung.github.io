3fed71901c7f111ab83648c75457b955effa09c9
Stacy Hung <sshung@gmail.com>
2020-09-24 20:40:44 -0700

    create layout and styling for basic carousel page for New Zealand trip

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
-// modify navigation button style
-
-/*
-document.getElementById("navbar").classList.toggle("menu");
-console.log(document.getElementById("navbar").className);
-
-var navbar = document.getElementById("navbar");
-navbar.style.alignItems = "center";
-navbar.style.justifyContent = "center";
-
-var navButtons = document.getElementsByClassName("navButton");
-//iterate through the array of navButtons and change the style for each one
-for (var i = 0; i < navButtons.length; i++) {
-    navButtons[i].style.color = "lavender";
-}
-
-// console.log(Object.keys(navButtons[0].style));*/
-
 // Event Listeners
-
-// When the user clicks on the input box
-// var inputBox = document.getElementById("guest-input");
-// inputBox.addEventListener("click", function () {
-//     this.value = "";
-//     this.style.color = "black";
-//     console.log("thank you for your input!")
-// });
-
-// // When the user clicks the submit button
-// var submitBtn = document.getElementById("submit-button");
-// submitBtn.addEventListener("click", function () {
-//     //clear the text box and bring up a text saying thank you <guest name>
-//     var guestName = document.getElementById("guest-input").value;
-//     document.getElementById("guest-input").value = "";
-//     document.getElementById("thankyou").textContent = "Thank you " + guestName + "!";
-// });
 
 // Produce a message when the user enters a valid email
 let submitForm = document.getElementById("suscribe-form");
@@ -49,3 +14,16 @@ submitForm.addEventListener("submit", event => {
     let updatedContent = `<p>Congratulations, ${userEmail} has been added! Keep an eye out for email updates!</p>`;
     submitForm.innerHTML = updatedContent;
 });
+
+// Buttons for carousel
+
+document.getElementById('btn-prev').addEventListener('click', prevImage);
+document.getElementById('btn-next').addEventListener('click', nextImage);
+
+function prevImage() {
+    //
+}
+
+function nextImage() {
+    //
+}
