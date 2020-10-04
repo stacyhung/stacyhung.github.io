4c7163f847ee60e0c64acd791e9bacc7b2195cdc
Stacy Hung <sshung@gmail.com>
2020-10-03 22:18:21 -0700

    create separate js file for main recipe page

---
+++ recipes.js
@@ -0,0 +1,28 @@
+/* Event listeners for main recipe page */
+
+(function () {
+
+    function init() {
+        var speed = 250,
+            easing = mina.easeinout;
+
+        [].slice.call(document.querySelectorAll('#grid > a')).forEach(function (el) {
+            var s = Snap(el.querySelector('svg')), path = s.select('path'),
+                pathConfig = {
+                    from: path.attr('d'),
+                    to: el.getAttribute('data-path-hover')
+                };
+
+            el.addEventListener('mouseenter', function () {
+                path.animate({ 'path': pathConfig.to }, speed, easing);
+            });
+
+            el.addEventListener('mouseleave', function () {
+                path.animate({ 'path': pathConfig.from }, speed, easing);
+            });
+        });
+    }
+
+    init();
+
+})();
